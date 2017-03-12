95fcff9b94c9085beb6fb0781716fd8cabc4abfa
Zhenyu Zhong <zacharyzhong1116@gmail.com>
2017-03-11 18:57:33 -0800

    Update app.py

--- app.py
+++ app.py
@@ -1,10 +1,10 @@
 from flask import Flask
-
+import requests
 app = Flask(__name__)
-
+url = "https://raw.github.com/zacharyzhong1116/cmpe273-assignment1/master/myyml.yml"
+c = requests.get(url).content
 @app.route("/")
 def hello():
-    return "Hello from Dockerized Flask App!!"
-
+    return c
 if __name__ == "__main__":
     app.run(debug=True,host='0.0.0.0')
